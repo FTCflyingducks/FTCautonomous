e1909bc8d9e6a05ffc37abcc3e9414396ac3ed65
FTCflyingducks <94571506+FTCflyingducks@users.noreply.github.com>
2021-12-15 16:18:45 -0500

    Create DuckSpinnerTeleop.java

---
+++ DuckSpinnerTeleop.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import java.util.Map;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+ @Autonomous(name="duckSpinner", group="DuckSpinner")
+public class DuckSpinnerTeleOp extends LinearOpMode { 
+     
+}
