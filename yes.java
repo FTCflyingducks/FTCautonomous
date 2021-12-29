46a2e7ee5fb6e48e9c1385cb4bd04d49588a8261
FTCflyingducks <94571506+FTCflyingducks@users.noreply.github.com>
2021-12-29 14:03:27 -0500

    Update yes.java

--- yes.java
+++ yes.java
@@ -9,16 +9,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
         static final double FORWARD_SPEED = 0.2;
         public Autonomous mode;{
         duckspinner = hardwareMap.get(DcMotor.class, "dusp");
-         if (red = 1){ 
+         if button(gamepad.x) 
              duckSpinner.setDirection(DcMotor.Direction.FORWARD);
-             
-         else;{
+         if button(gamepad.y)
             duckSpinner.setDirection(DcMotor.Direction.FORWARD);
-         }
         duckSpinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         duckSpinner.setPower(FORWARD_SPEED);
         while (opModeIsActive() && (runtime.seconds() < 1.0)){
-            set.
+            if button(gamepad.x) 
+             duckSpinner.setspeed(DcMotor.speed.FORWARD_SPEED);
+         if button(gamepad.y)
+            duckSpinner.setspeed -(DcMotor.speed.FORWARD_SPEED);
     }
     telemetry.update();
     sleep(1000);
