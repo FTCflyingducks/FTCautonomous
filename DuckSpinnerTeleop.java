7c7b5259fecad9bac97105175fbfa4a344bbb31f
FTCflyingducks <94571506+FTCflyingducks@users.noreply.github.com>
2022-01-07 08:44:38 -0500

    Update DuckSpinnerTeleop.java

--- DuckSpinnerTeleop.java
+++ DuckSpinnerTeleop.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-@TeleOp(name="duckSpinner", group="DuckSpinner")
+@TeleOp(name="DuckSpinnerTeleOp", group="DuckSpinner")
 public class DuckSpinnerTeleOp extends LinearOpMode {
     
     private DcMotor backLeftMotor;
@@ -22,58 +22,51 @@ public class DuckSpinnerTeleOp extends LinearOpMode {
         backRightMotor.setDirection(DcMotor.Direction.FORWARD);
         frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
         waitForStart();
-    
-        while (opModeIsActive()) {
-    
-            double px = gamepad1.left_stick_x;
-            double py = -gamepad1.left_stick_y;
-            double pa = (gamepad1.right_stick_x - gamepad1.right_stick_y);
-            if (Math.abs(pa) < 0.05) pa = 0;
-            double p1 = -px + py + pa;
-            double p2 = px + py + pa;
-            double p3 = -px + py - pa;
-            double p4 = px + py - pa;
-            double max = Math.max(1.0, Math.abs(p1));
-            max = Math.max(max, Math.abs(p2));
-            max = Math.max(max, Math.abs(p3));
-            max = Math.max(max, Math.abs(p4));
-            p1 /= max;
-            p2 /= max;
-            p3 /= max;
-            p4 /= max;
-
-    public DcMotor  duckSpinner   = null;
-    private ElapsedTime     runtime = new ElapsedTime();
+        public DcMotor  duckSpinner   = null;
+        private ElapsedTime     runtime = new ElapsedTime();
 
 
-    static final double     BLUE_SPEED = 0.5;
-    static final double     RED_SPEED = -0.5; 
-
-    @Override
-    public void runOpMode() {
+        static final double     BLUE_SPEED = 0.5;
+        static final double     RED_SPEED = -0.5; 
         duckSpinner  = hardwareMap.get(DcMotor.class, "dusp");
         duckSpinner.setDirection(DcMotor.Direction.REVERSE);
         duckSpinner.setPower(0);
         duckSpinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        waitForStart();
         duckSpinner.setPower(0.5);
         telemetry.addData("Path", "Running" + 0.5);
         telemetry.update();
-        while (opModeIsActive()) {
-            if (gamepad1.x) {
-                duckSpinner.setPower(BLUE_SPEED);
-                telemetry.addData("Path", "BLUE_SPEED");
-                telemetry.update();
-            } else if (gamepad1.a) {
-                duckSpinner.setPower(RED_SPEED);
-                telemetry.addData("Path", "RED_SPEED");
-                telemetry.update();
-            } else {
-                duckSpinner.setPower(0);
+            while (opModeIsActive()) {
+                double px = gamepad1.left_stick_x;
+                double py = -gamepad1.left_stick_y;
+                double pa = (gamepad1.right_stick_x - gamepad1.right_stick_y);
+                if (Math.abs(pa) < 0.05) pa = 0;
+                double p1 = -px + py + pa;
+                double p2 = px + py + pa;
+                double p3 = -px + py - pa;
+                double p4 = px + py - pa;
+                double max = Math.max(1.0, Math.abs(p1));
+                max = Math.max(max, Math.abs(p2));
+                max = Math.max(max, Math.abs(p3));
+                max = Math.max(max, Math.abs(p4));
+                p1 /= max;
+                p2 /= max;
+                p3 /= max;
+                p4 /= max;
+
+                if (gamepad1.x) {
+                    duckSpinner.setPower(BLUE_SPEED);
+                    telemetry.addData("BLUE_SPEED")
+                    telemetry.update();
+                } else if (gamepad1.a) {
+                    duckSpinner.setPower(RED_SPEED);
+                    telemetry.addData("RED_SPEED");
+                    telemetry.update();
+                } else {
+                    duckSpinner.setPower(0);
+                }
             }
+            telemetry.addData("Path", "Complete");
+            telemetry.update();
+            sleep(1000);
         }
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
-        sleep(1000);
     }
-}
