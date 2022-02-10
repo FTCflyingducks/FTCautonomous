d94e40df5a009a85dfd4678d048b09c76944b78b
FTCflyingducks <94571506+FTCflyingducks@users.noreply.github.com>
2022-02-10 11:38:42 -0500

    Update duckSpinnerAutoBlue.java

--- duckSpinnerAutoBlue.java
+++ duckSpinnerAutoBlue.java
@@ -1,35 +1,61 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.robot.Robot;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import java.util.Map;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Blinker;
 import com.qualcomm.robotcore.util.ElapsedTime;
- @Autonomous(name="duckSpinner", group="DuckSpinner")
-public class DuckSpinnerBlue extends LinearOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-    public DcMotor  duckSpinner   = null;
+@Autonomous(name = "auto", group = "Auto")
+
+public class Auto extends LinearOpMode{
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor; 
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor duckSpinner;
+    static double BLUE_SPEED = 0.7;
+    static double RED_SPEED = -0.7;
+    static double DUCK_SPEED = 0.4;
     private ElapsedTime     runtime = new ElapsedTime();
-
-
-    static final double     FORWARD_SPEED = 0.5;
-
-    @Override
     public void runOpMode() {
+        backLeftMotor = hardwareMap.get(DcMotor.class, "bl");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "fl");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "fr");
+        backRightMotor = hardwareMap.get(DcMotor.class, "br");
         duckSpinner  = hardwareMap.get(DcMotor.class, "dusp");
         duckSpinner.setDirection(DcMotor.Direction.FORWARD);
-        duckSpinner.setPower(0);
-        duckSpinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
-        duckSpinner.setPower(0.5);
-        duckSpinner.setPower(FORWARD_SPEED);
-        while (opModeIsActive() && (runtime.seconds() < 30.0)) {
-            telemetry.update();
-        }
-         telemetry.addData("Path", "Complete");
+        telemetry.addData("Path", "Running - " + 0.5);
         telemetry.update();
-        sleep(1000);
-    }
-}
+        while (opModeIsActive() &&runtime.seconds()<5){
+            frontLeftMotor.setPower(BLUE_SPEED);
+            backLeftMotor.setPower(BLUE_SPEED);
+            frontRightMotor.setPower(RED_SPEED);
+            backRightMotor.setPower(RED_SPEED);
+            while (opModeIsActive() && runtime.seconds()>3 & runtime.seconds()<5){
+                frontLeftMotor.setPower(RED_SPEED);
+                backLeftMotor.setPower(BLUE_SPEED);
+                frontRightMotor.setPower(RED_SPEED);
+                backRightMotor.setPower(BLUE_SPEED);
+                while (opModeIsActive() && runtime.seconds()>3.2 & runtime.seconds()<5.4){
+                    frontLeftMotor.setPower(-0.07);
+                    backLeftMotor.setPower(0.07);
+                    frontRightMotor.setPower(-0.07);
+                    backRightMotor.setPower(0.07);
+                    duckSpinner.setPower(DUCK_SPEED);
+                    
+                        while (opModeIsActive() && runtime.seconds()>5.3 & runtime.seconds()<6.272){
+                            frontLeftMotor.setPower(0.47);
+                            backLeftMotor.setPower(-0.47);
+                            frontRightMotor.setPower(0.47);
+                            backRightMotor.setPower(-0.47);
+                }
+            }
+        }
+    }}}
+        
